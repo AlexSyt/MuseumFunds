a6ecdf6f3e8bbf159706e35e7f3a2b08246e6750
Alex <asytnyk0@gmail.com>
2017-03-16 13:28:00 +0200

    set onClickListener for floatingActionButton

--- app/src/main/java/com/example/alex/museumfunds/MainActivity.java
+++ app/src/main/java/com/example/alex/museumfunds/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.alex.museumfunds;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.view.ContextMenu;
+import android.view.MenuItem;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,44 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        registerForContextMenu(fab);
+        fab.setOnClickListener((View view) -> openContextMenu(fab));
+    }
+
+    private static final int CM_ADD_EXHIBIT_ID = 1;
+    private static final int CM_ADD_AUTHOR_ID = 2;
+    private static final int CM_ADD_FUND_ID = 3;
+    private static final int CM_ADD_EXHIBITION_ID = 4;
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        menu.setHeaderTitle("Add:");
+        menu.add(0, CM_ADD_EXHIBIT_ID, 0, "Exhibit");
+        menu.add(0, CM_ADD_AUTHOR_ID, 0, "Author");
+        menu.add(0, CM_ADD_FUND_ID, 0, "Fund");
+        menu.add(0, CM_ADD_EXHIBITION_ID, 0, "Exhibition");
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case CM_ADD_EXHIBIT_ID:
+                return true;
+
+            case CM_ADD_AUTHOR_ID:
+                return true;
+
+            case CM_ADD_FUND_ID:
+                return true;
+
+            case CM_ADD_EXHIBITION_ID:
+                return true;
+
+            default:
+                return super.onContextItemSelected(item);
+        }
     }
 }
