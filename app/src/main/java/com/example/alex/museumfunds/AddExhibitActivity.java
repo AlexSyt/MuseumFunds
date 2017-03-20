2e6476436931b45ab463db39dc6e76684a67e38b
Alex <asytnyk0@gmail.com>
2017-03-20 19:01:44 +0200

    show alertDialog at button click

--- app/src/main/java/com/example/alex/museumfunds/AddExhibitActivity.java
+++ app/src/main/java/com/example/alex/museumfunds/AddExhibitActivity.java
@@ -1,7 +1,12 @@
 package com.example.alex.museumfunds;
 
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.TableLayout;
 
 public class AddExhibitActivity extends AppCompatActivity {
 
@@ -9,5 +14,49 @@ public class AddExhibitActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_exhibit);
+    }
+
+    public void addAuthorBtnClicked(View view) {
+        LayoutInflater inflater = getLayoutInflater();
+        View adOrganiserLayout = inflater.inflate(R.layout.add_author_ad, null);
+
+        AlertDialog adAuthor = new AlertDialog.Builder(this)
+                .setTitle("Add New Author")
+                .setView(adOrganiserLayout)
+                .setPositiveButton("Add", (dialog, id) -> {
+
+                })
+                .setNegativeButton("Cancel", null)
+                .create();
+        adAuthor.show();
+    }
+
+    public void addFundCatalogBtnClicked(View view) {
+        LayoutInflater inflater = getLayoutInflater();
+        View adFundCatalogView = inflater.inflate(R.layout.add_fund_catalog_ad, null);
+        final CheckBox cbNewFund = (CheckBox) adFundCatalogView.findViewById(R.id.new_fund_cb);
+        final TableLayout tlNewFund = (TableLayout) adFundCatalogView.findViewById(R.id.new_fund_tl);
+
+        cbNewFund.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (isChecked) {
+                tlNewFund.setVisibility(View.VISIBLE);
+            } else {
+                tlNewFund.setVisibility(View.GONE);
+            }
+        });
+
+        AlertDialog adFundCatalog = new AlertDialog.Builder(this)
+                .setTitle("Add New Fund Catalog")
+                .setView(adFundCatalogView)
+                .setPositiveButton("Add", (dialog, id) -> {
+
+                })
+                .setNegativeButton("Cancel", null)
+                .create();
+        adFundCatalog.show();
+    }
+
+    public void addExhibitionBtnClicked(View view) {
+
     }
 }
