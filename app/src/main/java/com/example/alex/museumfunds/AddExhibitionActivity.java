988dba19e1d896ca6e4ce5ff733ad5a8ff6f5dc9
Alex <asytnyk0@gmail.com>
2017-03-20 16:17:34 +0200

    show alertDialog at button click

--- app/src/main/java/com/example/alex/museumfunds/AddExhibitionActivity.java
+++ app/src/main/java/com/example/alex/museumfunds/AddExhibitionActivity.java
@@ -1,7 +1,10 @@
 package com.example.alex.museumfunds;
 
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
 
 public class AddExhibitionActivity extends AppCompatActivity {
 
@@ -9,5 +12,24 @@ public class AddExhibitionActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_exhibition);
+    }
+
+    public void addOrganiserBtnClicked(View view) {
+        LayoutInflater inflater = getLayoutInflater();
+        View adOrganiserLayout = inflater.inflate(R.layout.add_organiser_ad, null);
+
+        AlertDialog adOrganiser = new AlertDialog.Builder(this)
+                .setTitle("Add New Organiser")
+                .setView(adOrganiserLayout)
+                .setPositiveButton("Add", (dialog, id) -> {
+
+                })
+                .setNegativeButton("Cancel", null)
+                .create();
+        adOrganiser.show();
+    }
+
+    public void addExhibitBtnClicked(View view) {
+
     }
 }
