0594f149b763d0439aba9246b87b1e6bd8c4ed72
Alex <asytnyk0@gmail.com>
2017-03-21 18:20:58 +0200

    change fields

--- app/src/main/java/com/example/alex/museumfunds/model/Exhibition.java
+++ app/src/main/java/com/example/alex/museumfunds/model/Exhibition.java
@@ -4,14 +4,19 @@ import com.j256.ormlite.field.DataType;
 import com.j256.ormlite.field.DatabaseField;
 import com.j256.ormlite.table.DatabaseTable;
 
+import java.util.Date;
+
 @DatabaseTable(tableName = "exhibitions")
 public class Exhibition extends BaseColumns {
 
     @DatabaseField(canBeNull = false, foreign = true, foreignAutoRefresh = true)
     private Organiser organiser;
 
-    @DatabaseField(canBeNull = false, dataType = DataType.STRING)
-    private String duration;
+    @DatabaseField(canBeNull = false, dataType = DataType.DATE)
+    private Date startDate;
+
+    @DatabaseField(canBeNull = false, dataType = DataType.DATE)
+    private Date endDate;
 
     public Exhibition() {
 
@@ -25,11 +30,19 @@ public class Exhibition extends BaseColumns {
         this.organiser = organiser;
     }
 
-    public String getDuration() {
-        return duration;
+    public Date getStartDate() {
+        return startDate;
     }
 
-    public void setDuration(String duration) {
-        this.duration = duration;
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(Date endDate) {
+        this.endDate = endDate;
     }
 }
